15fe20d8da7edae405a1f0406c286789240244e3
a-korkin <a-korkin88@yandex.ru>
2026-06-09 19:25:28 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import csv
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Candle:
+    open: float
+    close: float
+    high: float
+    low: float
+    value: float
+    volume: int
+    begin: datetime
+    end: datetime
+
+    def __init__(self, obj: dict):
+        self.open = obj["open"]
+        self.close = obj["close"]
+        self.high = obj["high"]
+        self.low = obj["low"]
+        self.value = obj["value"]
+        self.volume = obj["volume"]
+        self.begin = obj["begin"]
+        self.end = obj["end"]
+
+    def __str__(self) -> str:
+        return f"open: {self.open}, close: {self.close}, begin: {self.begin}, end: {self.end}"
+
+    @staticmethod
+    def parse_file(filename: str) -> list:
+        with open(file=filename, mode="r", encoding="utf-8", newline="") as file:
+            reader = csv.DictReader(file, delimiter=";")
+            candles = []
+            for row in reader:
+                candles.append(Candle(row))
+
+            return candles
+
+
+def main():
+    filename = os.getenv("FILENAME") or ""
+    candles = Candle.parse_file(filename)
+    for candle in candles:
+        print(candle)
+
+
+if __name__ == "__main__":
+    main()
