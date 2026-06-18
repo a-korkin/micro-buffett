df8b45bbd374ab1615c340bffbaffa6630beeac6
a-korkin <a-korkin88@yandex.ru>
2026-06-18 15:43:19 +0300

    fix: ignore downloaded files

--- scripts/boundization.sh
+++ scripts/boundization.sh
@@ -2,15 +2,19 @@
 
 download_path="tests/data/boundization/"
 start=0
+step=50
 url="https://iss.moex.com/iss/statistics/engines/stock/markets/bonds/bondization.csv\
-?from=2026-07-01&till=2026-07-31&start=${start}&limit=20&iss.only=coupons\
+?from=2026-07-01&till=2026-07-31&start=${start}&limit=${step}&iss.only=coupons\
 &sort_order=desc&iss.json=extended&lang=ru&is_traded=1"
 
 # date -d "+5 days"
 # date +%F
 # curl -s https://example.com | iconv -f ISO-8859-1 -t UTF-8 > output.txt
+# echo $(( 1507 / 20 ))
+# echo $(( 1507 % 20 ))
+# coupons,coupons.cursor
 
 # curl -s -X GET ${url} | jq ".[1].coupons"
 # tail -n +3 tests/data/boundization/second.csv
 
-curl -s -X GET ${url} | iconv -f WINDOWS-1251  -t UTF-8 | tail -n +3 > ${download_path}tetst.csv
+curl -s -X GET ${url} | iconv -f WINDOWS-1251  -t UTF-8 | tail -n +3 > ${download_path}coupons.csv
