c5cc20780aca71bf77adeab859f735e0e899e88c
a-korkin <a-korkin88@yandex.ru>
2026-06-18 15:30:40 +0300

    fix: shell

--- scripts/boundization.sh
+++ scripts/boundization.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+download_path="tests/data/boundization/"
 start=0
-url="https://iss.moex.com/iss/statistics/engines/stock/markets/bonds/bondization.json?from=2026-07-01&till=2026-07-31&start=${start}&limit=20&iss.only=coupons&sort_order=desc&iss.json=extended&iss.meta=off&lang=ru&is_traded=1"
+url="https://iss.moex.com/iss/statistics/engines/stock/markets/bonds/bondization.csv\
+?from=2026-07-01&till=2026-07-31&start=${start}&limit=20&iss.only=coupons\
+&sort_order=desc&iss.json=extended&lang=ru&is_traded=1"
 
 # date -d "+5 days"
 # date +%F
 # curl -s https://example.com | iconv -f ISO-8859-1 -t UTF-8 > output.txt
 
-curl -s -X GET ${url} | jq ".[1].coupons"
+# curl -s -X GET ${url} | jq ".[1].coupons"
+# tail -n +3 tests/data/boundization/second.csv
+
+curl -s -X GET ${url} | iconv -f WINDOWS-1251  -t UTF-8 | tail -n +3 > ${download_path}tetst.csv
