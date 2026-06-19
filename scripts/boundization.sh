c3975a06c96320a6a925b6dd4ee867198b38658c
a-korkin <a-korkin88@yandex.ru>
2026-06-19 11:37:24 +0300

    fix: printing downloading status message

--- scripts/boundization.sh
+++ scripts/boundization.sh
@@ -6,21 +6,10 @@ from=$(date +%F)
 till=$(date -d"+1 months" +%F)
 dir="tests/data/boundization/${from}"
 base_url="https://iss.moex.com/iss/statistics/engines/stock/markets/bonds/bondization.csv"
-# url="${base_url}\
-# ?from=${from}&till=${till}&start=${start}&limit=${step}&iss.only=coupons.cursor\
-# &sort_order=desc&iss.json=extended&lang=ru&is_traded=1"
 
-# date -d "+5 days"
-# date +%F
-# curl -s https://example.com | iconv -f ISO-8859-1 -t UTF-8 > output.txt
-# echo $(( 1507 / 20 ))
-# echo $(( 1507 % 20 ))
-# coupons,coupons.cursor
-
-# curl -s -X GET ${url} | jq ".[1].coupons"
-# tail -n +3 tests/data/boundization/second.csv
-
-
+print_status() {
+    echo $(date +"%F %T") "[INFO]: url >>> ${url}"
+}
 
 check_dir_exists() {
     if [ -d $dir ]; then
@@ -34,7 +23,6 @@ download_csv() {
     url="${base_url}\
 ?from=${from}&till=${till}&start=${start}&limit=${step}&iss.only=${1}\
 &sort_order=desc&iss.json=extended&lang=ru&is_traded=1"
-    echo $url
     curl -s -X GET ${url} | iconv -f WINDOWS-1251  -t UTF-8 | tail -n +3 > ${dir}/${1}${2}.csv
 }
 
@@ -45,9 +33,8 @@ get_pages() {
 
 iterate_through_pages() {
     iteration=0
-
     while [ $start -lt $total ]; do
-        echo "start: ${start}, total: ${total}, iteration: ${iteration}"
+        print_status
         download_csv coupons "_${iteration}"
         sleep 5
         ((start += step))
