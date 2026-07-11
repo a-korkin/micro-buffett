94030fb647bebd552f3b1ac020d9c35e54a35684
a-korkin <a-korkin88@yandex.ru>
2026-07-11 20:05:16 +0300

    refactor: removed comments

--- terminal.py
+++ terminal.py
@@ -1,10 +1,8 @@
 import logging
 import math
 from datetime import datetime, timedelta
-from operator import index
 
 from pyray import (
-    Rectangle,
     Vector2,
     begin_drawing,
     clear_background,
@@ -31,8 +29,8 @@ FPS = 60
 BACKGROUND_COLOR = (39, 51, 56, 255)
 RED = (224, 84, 84, 255)
 GREEN = (43, 87, 72, 255)
-WIDTH = 1600
-HEIGHT = 900
+WIDTH = 1024  # 1600
+HEIGHT = 768  # 900
 GAP = 20
 DATETIME_FMT = "%Y-%m-%d %H:%M:%S"
 
@@ -151,7 +149,6 @@ class Graph:
                 self.maxc.end = candle.end
 
         # x-axe
-        # ====================================================================
         self.axe_x = Axe(self.center, self.bottom_right, self.minc, self.maxc)
         # TODO: проверять в каком формате интервалы (минуты, часы, дни)
         self.start = datetime.strptime(str(self.minc.begin), DATETIME_FMT) - timedelta(
@@ -173,18 +170,6 @@ class Graph:
         while start < self.stop:
             start += timedelta(minutes=1)
             x += self.step_x
-
-            # up = Vector2(x, y_pos - 5)
-            # down = Vector2(x, y_pos + 5)
-            #
-            # if start.minute % 5 == 0:
-            #     up = Vector2(x, y_pos - 7.5)
-            #     down = Vector2(x, y_pos + 7.5)
-            #     value = start.strftime("%H:%M")
-            #     draw_text(value, int(x) - 10, y_pos + 10, 8, WHITE)
-            #
-            # draw_line_ex(up, down, self.thick, BLACK)
-
             scale = Scale(
                 title=start.strftime("%H:%M"),
                 position=Vector2(x, y_pos),
@@ -193,7 +178,6 @@ class Graph:
             self.axe_x.scales.append(scale)
 
         # y-axe
-        # ====================================================================
         self.axe_y = Axe(self.up_left, self.center, self.minc, self.maxc)
         self.max_y = float(math.ceil(self.axe_y.max_value))
         self.min_y = float(math.floor(self.axe_y.min_value))
@@ -226,7 +210,6 @@ class Graph:
                 GRAPH.sum_to_coord(mmax),
             )
             size = Vector2(GRAPH.step_x, GRAPH.step_y * (max(mmax - mmin, GRAPH.thick)))
-            # rec = Rectangle(position.x, position.y, size.x, size.y)
 
             candle.position.x, candle.position.y = position.x, position.y
             candle.size.x, candle.size.y = size.x, size.y
@@ -244,32 +227,6 @@ class Graph:
                 right = Vector2(scale.position.x + 7.5, scale.position.y)
 
             draw_line_ex(left, right, self.thick, BLACK)
-
-        # self.max_y = float(math.ceil(self.axe_y.max_value))
-        # self.min_y = float(math.floor(self.axe_y.min_value))
-        #
-        # length = float(HEIGHT - GAP * 3)
-        # count_mark = int(self.max_y - self.min_y)
-        # self.step_y = length / count_mark
-        #
-        # y = HEIGHT - GAP * 2
-        #
-        # i = 0
-        # while y >= GAP:
-        #     y -= self.step_y
-        #     i += 1
-        #
-        #     left = Vector2(GAP * 3 - 5, y)
-        #     right = Vector2(GAP * 3 + 5, y)
-        #
-        #     if i % 2 == 0:
-        #         value = f"{float(self.min_y + i):.2f}"
-        #         draw_text(value, GAP - 10, int(y - 4), 8, WHITE)
-        #
-        #         left = Vector2(GAP * 3 - 7.5, y)
-        #         right = Vector2(GAP * 3 + 7.5, y)
-        #
-        #     draw_line_ex(left, right, self.thick, BLACK)
 
     def draw_scale_x(self):  # , min_d: datetime, max_d: datetime):
         # TODO: проверять в каком формате интервалы (минуты, часы, дни)
@@ -289,53 +246,15 @@ class Graph:
                 )
 
             draw_line_ex(up, down, self.thick, BLACK)
-        # self.start = datetime.strptime(str(min_d), DATETIME_FMT) - timedelta(minutes=1)
-        # start = self.start
-        # self.stop = datetime.strptime(str(max_d), DATETIME_FMT) + timedelta(
-        #     minutes=1, seconds=1
-        # )
-        #
-        # length = float(WIDTH - GAP * 4)
-        # time_diff = (self.stop - start).total_seconds() / 60
-        # count_mark = int(time_diff)
-        # self.step_x = length / count_mark
-        # x = GAP * 3
-        #
-        # y_pos = HEIGHT - GAP * 2
-        # while start < self.stop:
-        #     start += timedelta(minutes=1)
-        #     x += self.step_x
-        #
-        #     up = Vector2(x, y_pos - 5)
-        #     down = Vector2(x, y_pos + 5)
-        #
-        #     if start.minute % 5 == 0:
-        #         up = Vector2(x, y_pos - 7.5)
-        #         down = Vector2(x, y_pos + 7.5)
-        #         value = start.strftime("%H:%M")
-        #         draw_text(value, int(x) - 10, y_pos + 10, 8, WHITE)
-        #
-        #     draw_line_ex(up, down, self.thick, BLACK)
 
     def draw_axes(self):
         # y-axes
-        # draw_line_ex(
-        #     self.up_left,
-        #     self.center,
-        #     self.thick,
-        #     BLACK,
-        # )
         draw_line_ex(self.axe_y.start_point, self.axe_y.end_point, self.thick, BLACK)
 
-        # _min = min(self.minc.open, self.minc.close, self.minc.high, self.minc.low)
-        # _max = max(self.maxc.open, self.maxc.close, self.maxc.high, self.maxc.low)
         self.draw_scale_y()
 
         # x-axes
-        # draw_line_ex(self.center, self.bottom_right, self.thick, BLACK)
         draw_line_ex(self.axe_x.start_point, self.axe_x.end_point, self.thick, BLACK)
-        # _min = self.minc.begin
-        # _max = self.maxc.end
 
         self.draw_scale_x()  # min_d=_min, max_d=_max)
 
@@ -357,17 +276,7 @@ GRAPH = Graph(
 
 
 def draw_candle(candle: Candle):
-    # mmin = float(min(candle.open, candle.close))
-    # mmax = float(max(candle.open, candle.close))
-
-    # position = Vector2(
-    #     GRAPH.time_to_coord(candle.begin) - GRAPH.step_x / 2.0,
-    #     GRAPH.sum_to_coord(mmax),
-    # )
-    # size = Vector2(GRAPH.step_x, GRAPH.step_y * (max(mmax - mmin, GRAPH.thick)))
-    # rec = Rectangle(position.x, position.y, size.x, size.y)
-
-    color = GREEN if candle.open < candle.close else RED
+    color = GREEN if candle.open <= candle.close else RED
     draw_rectangle_rec(
         (candle.position.x, candle.position.y, candle.size.x, candle.size.y),
         color,
